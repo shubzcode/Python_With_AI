0ca80c4cdb8d6a0784b87e87f2298d6fabf1236b
SHUBHAM SONUNE <shubham8262@gmail.com>
2024-07-11 15:45:23 +0530

    new file added

---
+++ Chapter 6/02_if_elif_else_ladder.py
@@ -0,0 +1,13 @@
+a = int(input("Enter your Age : "))
+
+if(a>=18):
+    print("You are aboe the age of concent")
+    print("good to you")
+elif(a<0):
+    print("InValid Age plese enter positive age")
+elif(a==0):
+    print("You are entering 0 which is not a valid age")    
+else:
+    print("You are below age")
+    
+print("End Of Program")
