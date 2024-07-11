af12b83e6d82e57a0953d30392c8b38020dfa71a
SHUBHAM SONUNE <shubham8262@gmail.com>
2024-07-11 15:22:47 +0530

    dae

---
+++ Chapter 5 PS/problem6.py
@@ -0,0 +1,23 @@
+"""6. Create an empty dictionary. Allow 4 friends to enter their favorite language as
+value and use key as their names. Assume that the names are unique"""
+
+d = {}
+
+name = input("Enter friend name: ")
+lang = input("Enter Language name: ")
+d.update({name: lang})
+
+
+name = input("Enter friend name: ")
+lang = input("Enter Language name: ")
+d.update({name: lang})
+
+name = input("Enter friend name: ")
+lang = input("Enter Language name: ")
+d.update({name: lang})
+
+name = input("Enter friend name: ")
+lang = input("Enter Language name: ")
+d.update({name: lang})
+
+print(d)
