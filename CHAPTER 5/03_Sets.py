eddbbebcbe4e280b0bfff293ccf0679eaf5a3ff6
SHUBHAM SONUNE <shubham8262@gmail.com>
2024-07-11 09:20:19 +0530

    remove

--- CHAPTER 5/03_Sets.py
+++
@@ -1,5 +0,0 @@
-s = {1, 5, 32, 54, 5, 5, 5}
-
-e = set() # Dont use s = {} as it will create an empty dictionary
-
-print(s)
--- CHAPTER 5/04_set_Methods.py
+++
@@ -1,5 +0,0 @@
-s = {1, 5, 32, 54, 5, 5, 5, "Shubham", "Sonune"}
-
-print(s,type(s))
-s.add(5442)
-print(s,type(s))
