27c08c1c6f450342ec29d247f7356627c611f84a
SHUBHAM SONUNE <shubham8262@gmail.com>
2024-07-11 15:16:17 +0530

    ddd

--- Chapter 5 PS/problem1.py
+++ Chapter 5 PS/problem1.py
@@ -1,3 +1,11 @@
 # 1. Write a program to create a dictionary of Hindi words with values as their English
 # translation. Provide user with an option to look it up!
 
+words = {
+    "madat": "Help",
+    "Kursi": "Chair",
+    "Undir": "Cat"
+}
+
+word = input("enter the word You want meaning of: ")
+print(words[word])
---
+++ Chapter 5 PS/problem3.py
@@ -0,0 +1,6 @@
+# 3. Can we have a set with 18 (int) and '18' (str) as a value in it?
+s = set()
+s.add(18)
+s.add("18") # Yes
+
+print(s)
