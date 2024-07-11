092075d067a8d376bf98d932b7a2ade01220e8c0
SHUBHAM SONUNE <shubham8262@gmail.com>
2024-07-11 15:56:54 +0530

    Create 03_Multiple_If_Else_Statement.py

---
+++ Chapter 6/03_Multiple_If_Else_Statement.py
@@ -0,0 +1,12 @@
+# if elif else ladder
+a = int(input("Enter your Age : "))
+if(a%2 == 0):
+    print("a is even")
+if(a>=18):
+    print("You are aboe the age of concent")
+    print("good to you")
+
+else:
+    print("You are below age")
+    
+print("End Of Program")
