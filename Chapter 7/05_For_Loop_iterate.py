0ab1230fc2398678917c5b10a8c5987300d83a26
SHUBHAM SONUNE <shubham8262@gmail.com>
2024-08-07 17:15:52 +0530

    Update 05_For_Loop_iterate.py

--- Chapter 7/05_For_Loop_iterate.py
+++ Chapter 7/05_For_Loop_iterate.py
@@ -1,13 +1,13 @@
 ## For Loop with Lists
-# l = [1, 4, 6, 234, 6, 764]
+l = [1, 4, 6, 234, 6, 764]
 
-# for i in l:
-#     print(i)
+for i in l:
+    print(i)
 
 #For Loop with Tuples
-# t = (6, 231, 75, 122)
-# for i in t:
-#     print(i)
+t = (6, 231, 75, 122)
+for i in t:
+    print(i)
 
 #For Loop with String
 s = "Kingston"
