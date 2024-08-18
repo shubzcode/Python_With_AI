d63856dbd02162b590d54da3e4b6a4769fee9220
SHUBHAM SONUNE <shubham8262@gmail.com>
2024-08-19 00:03:40 +0530

    f
    f

--- Chapter 7 PS/problem6.py
+++ Chapter 7 PS/problem6.py
@@ -1,2 +1,7 @@
 #6. Write a program to calculate the factorial of a given number using for loop.
 n = int(input("Enter the number: "))
+product = 1
+for i in range(1, n+1):
+    product = product * 1
+    
+print(f"The Factorial of {n} is {product}")
